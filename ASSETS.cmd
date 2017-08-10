1892621cc5129499478843788b4f1cdda2c677d1
Emilian Cioca <ciocaemilian@hotmail.ca>
2017-08-09 23:34:11 -0400

    Fixed the Workspace's AssetManager shortcut not working
    - Replaced .lnk with a BATCH launcher

--- ASSETS.cmd
+++ ASSETS.cmd
@@ -2,7 +2,7 @@
 
 title Packing Assets
 pushd Workspace
-call AssetManager.exe.lnk --pack
+call "%JEWEL3D_PATH%\Tools\AssetManager\bin\Release\AssetManager.exe" --pack
 if errorlevel 1 (
    title Assets Failure
    popd
